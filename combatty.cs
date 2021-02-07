bae51b4d867174544b45ab56ed8d0f5df1e71f7e
TheMaster6417 <62356987+TheMaster6417@users.noreply.github.com>
2021-02-07 17:12:01 +0000

    Add files via upload

---
+++ combatty.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class combatty : MonoBehaviour
+{
+    public Transform attackpoint;
+    public float attackRange = 0.5f;
+    public LayerMask titan;
+
+    public int attackDamage = 100;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+
+    }
+    void Attack()
+    {
+        Collider[] hitEnemy =  Physics.OverlapSphere(attackpoint.position, attackRange, titan);
+
+        foreach(Collider enemy in hitEnemy)
+        {
+            enemy.GetComponent<titans>().TakeDamage(100);
+        }
+
+    }
+    
+    
+}
